4a7975681e77be246a98fac264a766bed7753ccf
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2020-05-20 00:15:33 -0400

    added Todos component, refactored App.js into a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import Todos from "./components/Todos";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Practice coding",
+        completed: false,
+      },
+    ],
+  };
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
